d754b9cd7874f044a251a98c34e2f8eb26c57b1d
Amanibhavam <iam@defn.sh>
2018-03-09 19:11:27 -0800

    zerotier allowed everywhere

--- imma-bastion/main.tf
+++ imma-bastion/main.tf
@@ -3,15 +3,6 @@ variable "env_sg" {}
 variable "env_efs_sg" {}
 variable "service_iam_role" {}
 
-resource "aws_security_group_rule" "zerotier_in" {
-  type              = "ingress"
-  from_port         = 9993
-  to_port           = 9993
-  protocol          = "udp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = "${var.service_sg}"
-}
-
 resource "aws_security_group_rule" "ping_everything" {
   type                     = "ingress"
   from_port                = 8
