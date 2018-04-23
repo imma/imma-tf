fb4c9fc1cf2e8772c85e5cab1e2c566fce0d8293
Amanibhavam <iam@defn.sh>
2018-04-23 14:25:00 -0700

    let nat ping and ssh into stuff

--- imma-nat/main.tf
+++ imma-nat/main.tf
@@ -2,3 +2,21 @@ variable "service_sg" {}
 variable "env_sg" {}
 variable "env_efs_sg" {}
 variable "service_iam_role" {}
+
+resource "aws_security_group_rule" "ping_everything" {
+  type                     = "ingress"
+  from_port                = 8
+  to_port                  = 0
+  protocol                 = "icmp"
+  source_security_group_id = "${var.service_sg}"
+  security_group_id        = "${var.env_sg}"
+}
+
+resource "aws_security_group_rule" "ssh_into_everything" {
+  type                     = "ingress"
+  from_port                = 22
+  to_port                  = 22
+  protocol                 = "tcp"
+  source_security_group_id = "${var.service_sg}"
+  security_group_id        = "${var.env_sg}"
+}
