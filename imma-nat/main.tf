658249b0af85c4b679f1dbf5373a84d8d15c34cc
Amanibhavam <iam@defn.sh>
2018-05-14 00:45:01 -0700

    Revert "allow zerotier in"
    This reverts commit 829a8cc528d4e8cb5836f4d5adaf18a00747bda8.

--- imma-nat/main.tf
+++ imma-nat/main.tf
@@ -22,13 +22,3 @@ resource "aws_security_group_rule" "ssh_into_everything" {
   security_group_id        = "${var.env_sg}"
   description              = "nat can ssh to everything"
 }
-
-resource "aws_security_group_rule" "allow_zerotier" {
-  type              = "ingress"
-  from_port         = 9993
-  to_port           = 9993
-  protocol          = "udp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = "${aws_security_group.env.id}"
-  description       = "zerotier inbound"
-}
